82a368ddfd5c148895241da851202540b95b68a9
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2013-10-03 02:54:35 +0000

    Building docker in the container

--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
-
-### assumes go is installed and $GOROOT is already set
-
-GOPATH=`pwd`/go
-mkdir go
-rm docker
-
-go get github.com/fkautz/docker
-pushd go/src/github.com/dotcloud/docker
+git clone https://github.com/dotcloud/docker.git
+cd docker
+git fetch origin
 git remote add patches https://github.com/fkautz/docker.git
 git fetch patches
 git checkout v0.6.3
-git cherry-pick patches/v0.6.3-offline-image-transfer
-go install github.com/dotcloud/docker/docker
-popd
-cp $GOPATH/bin/docker .
-
+git merge patches/v0.6.3-offline-image-transfer
+docker build -t docker .
+docker run -lxc-conf=lxc.aa_profile=unconfined -privileged -v `pwd`:/go/src/github.com/dotcloud/docker docker hack/make.sh binary ubuntu
+if [ -e ./bundles/0.6.3/binary/docker-0.6.3 ]
+then
+  service docker stop
+  cp ./bundles/0.6.3/binary/docker-0.6.3 /usr/bin/docker
+  service docker start
+fi
